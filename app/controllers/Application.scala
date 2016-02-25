abdf93cf5eee5aaaa1f8f11ad6487dd7d6adbdda
tkt0821 <tkt0821@gmail.com>
2016-02-26 00:28:20 +0900

    フォーム値の受け渡し（GET）を実装

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,27 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+
+case class Csv(title: String, comment: String)
+
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+
+  val csvForm = Form(
+    mapping(
+      "title" -> text,
+      "comment" -> text
+    )(Csv.apply)(Csv.unapply)
+  )
+  def formSample = Action { implicit request =>
+    val csv = csvForm.bindFromRequest.get
+    Ok(csv.title)
   }
 
 }
